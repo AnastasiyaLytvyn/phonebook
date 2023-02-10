054c2d21e32f9204e18029097a7968770ded90ee
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2023-02-10 11:52:33 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,6 @@
-// import { ContactForm } from './ContactForm/ContactForm';
-// import { Filter } from './Filter/Filter';
-// import { ContactList } from './ContactList/ContactList';
-// import { Container, TitlePhone, TitleContacts } from './App.styled';
 import { Route, Routes } from 'react-router-dom';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { selectIsLoading } from 'redux/selectors';
-// import { useEffect } from 'react';
-// import { fetchContacts } from 'redux/operations';
-// import { RotatingLines } from 'react-loader-spinner';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import { Layout } from './Layout';
 import { Home } from 'pages/Home';
 import { Register } from 'pages/Register';
@@ -15,14 +8,19 @@ import { Login } from 'pages/Login';
 import { Contacts } from 'pages/Contacts';
 import { PrivateRoute } from './PrivateRoute';
 import { PublicRoute } from './PublicRoute';
+import { refreshUser } from 'redux/auth/operations';
 
 export function App() {
-  
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
 
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />}></Route>
+        <Route index element={<Home />} />
         <Route
           path="/register"
           element={
@@ -31,9 +29,7 @@ export function App() {
         ></Route>
         <Route
           path="/login"
-          element={
-            <PublicRoute redirectTo="/contacts" component={<Login />} />
-          }
+          element={<PublicRoute redirectTo="/contacts" component={<Login />} />}
         ></Route>
         <Route
           path="/contacts"
@@ -42,24 +38,6 @@ export function App() {
           }
         ></Route>
       </Route>
-
-      {/* <Container>
-      <TitlePhone>Phonebook</TitlePhone>
-      <ContactForm />
-      <TitleContacts>Contacts</TitleContacts>
-
-      <Filter />
-      {isLoading && (
-        <RotatingLines
-          strokeColor="grey"
-          strokeWidth="5"
-          animationDuration="0.75"
-          width="26"
-          visible={true}
-        />
-      )}
-      <ContactList />
-    </Container> */}
     </Routes>
   );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,14 @@
 import { NavLink } from 'react-router-dom';
+import { useAuth } from 'hooks';
 import { Container } from './Navigation.styled';
 
 export const Navigation = () => {
+  const { isLoggedIn } = useAuth();
+
   return (
     <Container>
       <NavLink to="/">Home</NavLink>
-      <NavLink to="/contacts">Contacts</NavLink>
+      {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
     </Container>
   );
 };
